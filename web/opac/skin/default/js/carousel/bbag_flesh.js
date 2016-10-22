46ecc82012657f949525c58c038a86d252ca034f
Brent <brent@hoodriverlibrary.org>
2016-10-22 13:28:07 -0700

    carousel fixes esi

--- web/opac/skin/default/js/carousel/bbag_flesh.js
+++ web/opac/skin/default/js/carousel/bbag_flesh.js
@@ -2,58 +2,66 @@
 /* maybe also a 'skin' var */
 
 function bbInit(bb) {
-	var cgi	= new CGI();	
-	if(!bb) { return; }
-	var req = new Request(FLESH_PUBLIC_CONTAINER, 'biblio', bb);
-	req.callback( bbShow );
-	req.send();
+  if(!bb) { return; }
+  new OpenSRF.ClientSession('open-ils.actor').request({
+    method: 'open-ils.actor.container.public.flesh',
+    params: ['biblio', bb],
+    async: true,
+    oncomplete: bbShow
+  }).send();
 }
 
 function bbShow(r) {
 
-	var bb = r.getResultObject();	
-	if(!bb || !bb.pub()) { return; }
-	var thisid = bb.id();	
-	bb_total[thisid]=bb.items().length;
-	
-	$('bb_name_'+thisid).appendChild(text(bb.name()));
+  var resp = r.recv();
+  if (!resp) { return; }
+  var bb = resp.content();
+  if(!bb || !bb.pub()) { return; }
+  var thisid = bb.id(); 
+  bb_total[thisid]=bb.items().length;
+  
+  $('bb_name_'+thisid).appendChild(text(bb.name()));
 
-	var tbody = $('bbitems_'+thisid);
-	
-	if(!template[thisid]) 	
-		template[thisid] = tbody.removeChild($('row_template_'+thisid));
-	
-	for( var i in bb.items() ) 
-		tbody.appendChild(bbShowItem( template[thisid], bb.items()[i] ));
+  var tbody = $('bbitems_'+thisid);
+  
+  if(!template[thisid])   
+    template[thisid] = tbody.removeChild($('row_template_'+thisid));
+  
+  for( var i in bb.items() ) 
+    tbody.appendChild(bbShowItem( template[thisid], bb.items()[i] ));
 }
 
 function bbShowItem( template, item ) {
-	var row = template.cloneNode(true);
+  var row = template.cloneNode(true);
+  var tlink = $n(row, 'title');
+  var alink = $n(row, 'author');  
 
-	var req = new Request( FETCH_RMODS, item.target_biblio_record_entry() );
-	req.request.tlink = $n(row, 'title');
-	req.request.alink = $n(row, 'author');	
-
-	req.callback( function(r) { 
-		var rec = r.getResultObject();
-		buildTitleDetailLink(rec, r.tlink); 
-		r.tlink.setAttribute('href', ''+rec.doc_id());
-		r.alink.appendChild(text(rec.author()));
-	});
-
-	req.send();
-	return row;
+  new OpenSRF.ClientSession('open-ils.search').request({
+    method: 'open-ils.search.biblio.record.mods_slim.retrieve',
+    params: [item.target_biblio_record_entry()],
+    aysnc: true,
+    oncomplete: function(r) {
+      var resp = r.recv();
+      if (!resp) { return; }
+      var rec = resp.content();
+      buildTitleDetailLink(rec, tlink); 
+      tlink.setAttribute('href', ''+rec.doc_id());
+      alink.appendChild(text(rec.author()));
+    }
+  }).send();
+    
+  return row;
 }
 
 jQuery(document).ready(function(){
-	for(var i=0;i<bbags.length;i++){
-	bb_total[bbags[i]] = 10000000;
-		jQuery('#hidden_bb_info').append(
-		"<div id='bbitems_"+bbags[i]+"'><div id='row_template_"+bbags[i]+"' class='bbitem_"+bbags[i]+"'><a href='#' name='title' class='bbtitle_"+bbags[i]+"'> </a><span name='author'> </span></div></div>");
-		jQuery('#carousels').append(
-		"<div><div id='bb_name_"+bbags[i]+"' class='carousel_title'> </div><div class='wrap'>  <ul id='mycarousel_"+bbags[i]+"' class='jcarousel-skin-meskin'>  </ul></div></div>");
-		
-		
-		bbInit(bbags[i]);
-	}
+  for(var i=0;i<bbags.length;i++){
+  bb_total[bbags[i]] = 10000000;
+    jQuery('#hidden_bb_info').append(
+    "<div id='bbitems_"+bbags[i]+"'><div id='row_template_"+bbags[i]+"' class='bbitem_"+bbags[i]+"'><a href='#' name='title' class='bbtitle_"+bbags[i]+"'> </a><span name='author'> </span></div></div>");
+    jQuery('#carousels').append(
+    "<div><div id='bb_name_"+bbags[i]+"' class='carousel_title'> </div><div class='wrap'>  <ul id='mycarousel_"+bbags[i]+"' class='jcarousel-skin-meskin'>  </ul></div></div>");
+    
+    
+    bbInit(bbags[i]);
+  }
 });
